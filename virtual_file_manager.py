33509ec2e856ae5b6ee4d8eb046b813bbe0e9555
sachinlodhi <sachinlodhi8614@gmail.com>
2024-12-28 21:30:33 -0800

    Extraction of the ROI

--- virtual_file_manager.py
+++ virtual_file_manager.py
@@ -1,4 +1,4 @@
-''' This file is going to work on the senders side to select files'''
+''' This file is going to work on the sender side to select files'''
 import subprocess
 from mss import mss
 import cv2
@@ -7,11 +7,11 @@ import time
 import glob
 import mediapipe as mp
 
-# For now let us stick to the constant screenshot and we will fix the screenshot process later
 
 # Now open the camera for image superimposition
 cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
 cv2.namedWindow("file manager", cv2.WINDOW_NORMAL)
+cv2.namedWindow("selected area", cv2.WINDOW_NORMAL)
 cap = cv2.VideoCapture(0)
 
 # loading file manager screen
@@ -23,7 +23,7 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-# function to extract the top of the index finger
+# function to extract the top of the index finger and draw it
 def landmarks_extraction(hand_landmarks):
    
     index_finger_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
@@ -49,21 +49,121 @@ def gesture_detection(hand_landmarks):
 
     ''' let us analyze if the palm is being shown. The hypothesis is that the y coordiante of the each fingers TIP will be less than PIP in open position'''
     # print(index_pip.y>index_tip.y, mid_pip.y<mid_tip.y, ring_pip.y<ring_tip.y, pinky_pip.y<pinky_tip.y)
-    # all fingers open
+    
+    '''codes
+    -1: palm,
+    0: fist,
+    1: index,
+    2: 2 fingers open
+    '''
+    # palm
     if index_pip.y>index_tip.y and mid_pip.y>mid_tip.y and ring_pip.y>ring_tip.y and pinky_pip.y>pinky_tip.y:
-        return True
-    # fist option
+        return -1
+    # fist
     elif index_pip.y<index_tip.y and mid_pip.y<mid_tip.y and ring_pip.y<ring_tip.y and pinky_pip.y<pinky_tip.y:
-        return True
-    # index fingers are being shown
-    elif index_pip.y>index_tip.y and mid_pip.y<mid_tip.y and ring_pip.y<ring_tip.y and pinky_pip.y<pinky_tip.y: # if only index finger is shown
-        return False
-    # else:
-    #     return False
+        return 0
+    # index finger
+    elif index_pip.y>index_tip.y and mid_pip.y<mid_tip.y and ring_pip.y<ring_tip.y and pinky_pip.y<pinky_tip.y:
+        return 1
+    # detect index and middle finger open to proceed with the selected files
+    elif index_pip.y>index_tip.y and mid_pip.y>mid_tip.y and ring_pip.y<ring_tip.y and pinky_pip.y<pinky_tip.y: 
+        return 2
 
-    # print(index_finger_mcp.y, index_finger_tip.y)
 
-points_to_draw = []
+
+
+
+'''function to get the selected area, process it using OCR and extract the filename'''
+    
+def get_filenames(img, area_pts):
+    ''' section to extract the roi'''
+    # creating new black and white image with curve that will serve as mask to extract roi
+    img_shape = img.shape[:2]
+    new_img = np.zeros(img_shape, dtype=np.uint8)
+    # setting boundary to white
+    for pt in area_pts:
+        x, y = pt 
+        new_img[y, x] = 255 
+
+    # creating mask
+    _, binary_mask = cv2.threshold(new_img, 0, 255, cv2.THRESH_BINARY) 
+    filled_mask = binary_mask.copy()
+    cv2.floodFill(filled_mask, None, (0, 0), 255) # Filling outside of the curve
+    filled_mask = cv2.bitwise_not(filled_mask) # Inversion of the mask
+
+    # Applying mask to the colored image 
+    masked_img = cv2.bitwise_and(img, img, mask=filled_mask)
+
+    # Getting bounding box
+    contours, _ = cv2.findContours(filled_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if len(contours) > 0:
+        largest_contour = max(contours, key=cv2.contourArea)
+        x, y, w, h = cv2.boundingRect(largest_contour)
+        roi = masked_img[y:y+h, x:x+w] 
+
+    return roi
+    
+    
+
+'''function to get the list of the connecting points between two points pt1<->pt2'''
+def get_line_points(start, end):
+    """
+    Get all points on a line between two points using Bresenham's algorithm.
+    
+    Parameters:
+        start: tuple of (x1, y1)
+        end: tuple of (x2, y2)
+    
+    Returns:
+        List of tuples containing (x, y) coordinates of all points on the line
+    """
+    x1, y1 = start
+    x2, y2 = end
+    
+    points = []
+    dx = abs(x2 - x1)
+    dy = abs(y2 - y1)
+    
+    # Determine direction of line
+    x_step = 1 if x1 < x2 else -1
+    y_step = 1 if y1 < y2 else -1
+    
+    # Determine whether to drive the algorithm by x or y
+    if dx > dy:
+        # Drive by x
+        err = dx / 2
+        x = x1
+        y = y1
+        
+        while x != x2 + x_step:
+            points.append((x, y))
+            err -= dy
+            if err < 0:
+                y += y_step
+                err += dx
+            x += x_step
+    else:
+        # Drive by y
+        err = dy / 2
+        x = x1
+        y = y1
+        
+        while y != y2 + y_step:
+            points.append((x, y))
+            err -= dx
+            if err < 0:
+                x += x_step
+                err += dy
+            y += y_step
+    
+    return points
+
+
+
+
+points_to_draw = []  # stores the mediapipe relative points
+points_file_manager = [] # stores the points to be drawn on the file_manager
+
 
 with mp_hands.Hands(
     static_image_mode=False,
@@ -74,9 +174,9 @@ with mp_hands.Hands(
     
     while True:
         _, frame = cap.read()
+        # For now let us stick to the constant screenshot and we will fix the screenshot process later
         file_manager = cv2.imread("manager_screen.png")
-        # print(frame.shape)
-        
+  
         frame_height, frame_width = frame.shape[0], frame.shape[1]
         file_manager_height, file_manager_width = file_manager.shape[0], file_manager.shape[1]
         frame.flags.writeable = False
@@ -92,26 +192,46 @@ with mp_hands.Hands(
 
                 x = index_tip.x 
                 y = index_tip.y
+                # circle to track the hand movement
                 cv2.circle(img=file_manager,center=(int(abs(1-x) * file_manager_width), int(y * file_manager_height)), radius=5, color=(0,0,255), thickness=-1 )
                 # append x,y to the list to draw things
                 points_to_draw.append((x,y))
-                try: # check if the palm is being shown
-                    if gesture_detection(hand_landmarks):
+                try: 
+                    if gesture_detection(hand_landmarks) in [0,-1]: # if palm  or first is being shown then clear every points in each list
                         points_to_draw.clear()
+                        points_file_manager.clear()
                         print("Tru")
+                    
+                    elif gesture_detection(hand_landmarks) == 2: # if 2 fingers are shown then get the cutout
+                        
+                        
+                        selected_files = get_filenames(img=file_manager, area_pts = points_file_manager)
+                        # print(selected_files)
+                        cv2.imshow("selected area", selected_files)
+
                     # iterate over the list to draw over the image
                     for idx in range(1, len(points_to_draw)):
-                        # print(points_to_draw[idx])
+                      
                         # recalculate pt1 and pt2 for the camera and file manager draw
                         frame_pt1 = (int(points_to_draw[idx][0]*frame_width), int(points_to_draw[idx][1]*frame_height))
                         frame_pt2 = (int(points_to_draw[idx-1][0] * frame_width), int(points_to_draw[idx-1][1] * frame_height))
 
                         file_manager_pt1 = (int(abs(1-points_to_draw[idx][0])*file_manager_width), int(points_to_draw[idx][1] * file_manager_height))
                         file_manager_pt2 = (int(abs(1-points_to_draw[idx-1][0])*file_manager_width), int(points_to_draw[idx-1][1] * file_manager_height))
+                        points_file_manager.append(file_manager_pt1)
 
                         cv2.line(img=frame, pt1= frame_pt1, pt2 = frame_pt2, color=(0, 0, 255), thickness=5)
                         cv2.line(img=file_manager, pt1= file_manager_pt1, pt2 = file_manager_pt2, color=(0, 0, 255), thickness=5)
-                        # cv2.circle(frame, (point[0], point[1]), radius=5, color=(0, 0, 255), thickness=-1) 
+                        
+                        # get the intermediate points on the line connecting pt1 and pt2
+                        line_points = get_line_points(start=file_manager_pt1, end=file_manager_pt2)
+                        print("appending", len(points_file_manager))
+                        for i in line_points:
+                            points_file_manager.append(i)
+                        print("appended", len(points_file_manager))
+                
+                   
+
                 except Exception as e:
                     print(f"Error: {e}")
                     pass
